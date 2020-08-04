f158ec94011eff46fc92673e1647800afa25cb7e
Mohit Rane <mrrane99@gmail.com>
2020-08-04 14:58:19 +0530

    Custom component and Header Styling

---
+++ src/screens/ComponentsScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Text, StyleSheet } from 'react-native'
+
+const ComponentsScreen = () => {
+    return (
+        <Text style={styles.textStyle}>This is ComponentsScreen</Text>
+    )
+}
+
+const styles = StyleSheet.create({
+    textStyle: {
+        fontSize: 30,
+    }
+})
+export default ComponentsScreen
