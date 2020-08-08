0a200de3b82628035fdc6bda34600195618da5d5
Mohit Rane <mrrane99@gmail.com>
2020-08-08 15:32:13 +0530

    Simplyfying with switch statement

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -8,21 +8,37 @@ const SquareScreen = () => {
     const [green, setGreen] = useState(0)
     const [blue, setBlue] = useState(0)
 
+    const setColor = (color, change) => {
+        switch (color){
+            case 'red': 
+                (red+change > 255 || red+change < 0)? null: setRed(red+change);
+                return;
+            case 'green': 
+                (green+change > 255 || green+change < 0)? null: setGreen(green+change);
+                return;
+            case 'blue': 
+                (blue+change > 255 || blue+change < 0)? null: setBlue(blue+change);
+                return;
+            default:
+                return;
+        }
+    };
+
     return (
         <View>
             <ColorCounter 
-                onIncrease={()=> setRed(red+COLOR_INCREMENT)} 
-                onDecrease={()=> setRed(red-COLOR_INCREMENT)} 
+                onIncrease={()=> setColor('red', COLOR_INCREMENT)} 
+                onDecrease={()=> setColor('red', -1*COLOR_INCREMENT)} 
                 color='red'
             />
             <ColorCounter 
-                onIncrease={()=> setGreen(green+COLOR_INCREMENT)} 
-                onDecrease={()=> setGreen(green-COLOR_INCREMENT)} 
+                onIncrease={()=> setColor('green', COLOR_INCREMENT)} 
+                onDecrease={()=> setColor('green', -1*COLOR_INCREMENT)} 
                 color='green'
             />
             <ColorCounter 
-                onIncrease={()=> setBlue(blue+COLOR_INCREMENT)} 
-                onDecrease={()=> setBlue(blue-COLOR_INCREMENT)} 
+                onIncrease={()=> setColor('blue', COLOR_INCREMENT)} 
+                onDecrease={()=> setColor('blue', -1*COLOR_INCREMENT)} 
                 color='blue'
             />
             <View style={styles.colorContainerStyle}>
