4ced4c4d8806ede47e033f65b211f1fe4d7a206c
Mohit Rane <mrrane99@gmail.com>
2020-08-09 16:18:29 +0530

    SquareScreen Reducer is used

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,52 +1,48 @@
-import React, {useState} from 'react'
-import { View, Button, StyleSheet, FlatList, Text } from 'react-native'
+import React, { useReducer } from 'react'
+import { View, StyleSheet, Text } from 'react-native'
 import ColorCounter from '../components/ColorCounter'
 
 const SquareScreen = () => {
     const COLOR_INCREMENT = 10
-    const [red, setRed] = useState(0)
-    const [green, setGreen] = useState(0)
-    const [blue, setBlue] = useState(0)
-
-    const setColor = (color, change) => {
-        switch (color){
+    
+    const reducer = (state, action)=>{
+        switch(action.colorToChange){
             case 'red': 
-                (red+change > 255 || red+change < 0)? null: setRed(red+change);
-                return;
+                return (state.red + action.amount > 255 || state.red + action.amount < 0)? state: {...state, 'red': state.red + action.amount};
             case 'green': 
-                (green+change > 255 || green+change < 0)? null: setGreen(green+change);
-                return;
+                return (state.green + action.amount > 255 || state.green + action.amount < 0)? state: {...state, 'green': state.green + action.amount};
             case 'blue': 
-                (blue+change > 255 || blue+change < 0)? null: setBlue(blue+change);
-                return;
-            default:
-                return;
+                return (state.blue + action.amount > 255 || state.blue + action.amount < 0)? state: {...state, 'blue': state.blue + action.amount};
+            default: 
+                return state;
         }
-    };
+    }
+
+    const [state, dispatch] = useReducer(reducer, {'red': 0, 'green': 0, 'blue': 0})
 
     return (
         <View>
             <ColorCounter 
-                onIncrease={()=> setColor('red', COLOR_INCREMENT)} 
-                onDecrease={()=> setColor('red', -1*COLOR_INCREMENT)} 
+                onIncrease={()=> dispatch({ colorToChange: 'red', amount: COLOR_INCREMENT})} 
+                onDecrease={()=> dispatch({ colorToChange: 'red', amount: -1*COLOR_INCREMENT})} 
                 color='red'
             />
             <ColorCounter 
-                onIncrease={()=> setColor('green', COLOR_INCREMENT)} 
-                onDecrease={()=> setColor('green', -1*COLOR_INCREMENT)} 
+                onIncrease={()=> dispatch({ colorToChange: 'green', amount: COLOR_INCREMENT})} 
+                onDecrease={()=> dispatch({ colorToChange: 'green', amount: -1*COLOR_INCREMENT})} 
                 color='green'
             />
             <ColorCounter 
-                onIncrease={()=> setColor('blue', COLOR_INCREMENT)} 
-                onDecrease={()=> setColor('blue', -1*COLOR_INCREMENT)} 
+                onIncrease={()=> dispatch({ colorToChange: 'blue', amount: COLOR_INCREMENT})} 
+                onDecrease={()=> dispatch({ colorToChange: 'blue', amount: -1*COLOR_INCREMENT})} 
                 color='blue'
             />
             <View style={styles.colorContainerStyle}>
-                <View style={{...styles.colorSquareStyle, backgroundColor:`rgb(${red}, ${green}, ${blue})`}}/>
+                <View style={{...styles.colorSquareStyle, backgroundColor:`rgb(${state.red}, ${state.green}, ${state.blue})`}}/>
             </View>
 
             <Text style={styles.bottomTextStyle}>
-                Red: {red}, Green: {green}, Blue: {blue}
+                Red: {state.red}, Green: {state.green}, Blue: {state.blue}
             </Text>
         </View>
     )
